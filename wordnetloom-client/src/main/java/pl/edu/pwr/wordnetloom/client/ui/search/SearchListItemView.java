7fff6dab96e405e484de2c7311e9d7b9c14f6b0c
Roman Dyszlewski <rdyszlewski@gmail.com>
2019-03-05 14:14:58 +0100

    Tooltip delay

--- wordnetloom-client/src/main/java/pl/edu/pwr/wordnetloom/client/ui/search/SearchListItemView.java
+++ wordnetloom-client/src/main/java/pl/edu/pwr/wordnetloom/client/ui/search/SearchListItemView.java
@@ -2,6 +2,7 @@ package pl.edu.pwr.wordnetloom.client.ui.search;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
@@ -15,6 +16,9 @@ import java.util.TimerTask;
 
 
 public class SearchListItemView implements FxmlView<SearchListItemViewModel> {
+
+	private final int TOOLTIP_DELAY = 1000;
+	private final int TOOLTIP_PERIOD = 5000;
 
 	@FXML
 	public Label mainLabel;
@@ -32,6 +36,10 @@ public class SearchListItemView implements FxmlView<SearchListItemViewModel> {
 
 	private Tooltip tooltip;
 
+	private Boolean mouseEntered = false;
+	private boolean tooltipLoaded = false;
+
+
 	public void initialize() {
 		mainLabel.textProperty().bind(viewModel.labelProperty());
 		initTooltip();
@@ -45,9 +53,27 @@ public class SearchListItemView implements FxmlView<SearchListItemViewModel> {
 
 	private void initTooltip() {
 		tooltip = new ListTooltip();
-		tooltip.activatedProperty().addListener(observable -> {
-			tooltip.setText(viewModel.getTooltipText());
-        });
+
+		mainLabel.addEventFilter(MouseEvent.MOUSE_ENTERED, event->{
+			mouseEntered = true;
+			if (!tooltipLoaded) {
+				new Timer().schedule(
+						new TimerTask() {
+							@Override
+							public void run() {
+								Platform.runLater(()->{
+									if(tooltip.getText().isEmpty() && mouseEntered){
+										tooltip.setText(viewModel.getTooltipText());
+										tooltipLoaded = true;
+									}
+								});
+							}
+						}, TOOLTIP_DELAY, TOOLTIP_PERIOD
+				);
+			}
+		});
+		mainLabel.addEventFilter(MouseEvent.MOUSE_EXITED, event -> mouseEntered = false);
+
 		Tooltip.install(mainLabel, tooltip);
 	}
 }

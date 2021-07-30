31032b357f1bf495e893c5ede010297961fb438a
Łukasz Szpak <lukaszszpak@e-science.pl>
2021-07-30 10:51:38 +0200

    Add quarkus and docker config

--- build-server.sh
+++ build-server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 docker-compose run --rm maven clean
 docker-compose run --rm maven package
-docker-compose build wildfly
+docker-compose build quarkus
+docker-compose run --publish 8080:8080 -d quarkus
